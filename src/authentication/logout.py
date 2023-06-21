7a68a09c6467c3eea89634536492073e1a6dba7c
DJMogens <dani788c@gmail.com>
2023-06-21 19:12:44 +0200

    Logout: delete cookie on server

--- src/authentication/logout.py
+++ src/authentication/logout.py
@@ -4,5 +4,6 @@ from flask import make_response, redirect, url_for
 
 def logout():
     response = make_response(redirect(url_for('login')))
-    response.set_cookie('sagalabs_auth', '', expires=0)
+    response.set_cookie('sagalabs_auth', '', expires=0) # Deletes localhost and 127.0.0.1 cookie
+    response.set_cookie('sagalabs_auth', '', expires=0, path='/', domain='.sagalabs.dk', secure=True) #Deletes cookie from backbone server
     return response
